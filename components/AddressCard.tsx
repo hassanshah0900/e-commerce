5f5bd76d0f9b371126697e2fd2aead3f03eb2d91
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-05-07 00:16:41 +0500

    feat(components): add AddressCard component

---
+++ components/AddressCard.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { FiPhone } from "react-icons/fi";
+export interface Address {
+  title: string;
+  firstName: string;
+  lastName: string;
+  city: string;
+  phone: string;
+  address: string;
+}
+
+export default function AddressCard({ address }: { address: Address }) {
+  return (
+    <div className="bg-white rounded-lg p-6 text-body-dark flex">
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold mb-3">{address.title}</h2>
+        <div>{address.city}</div>
+        <div>{address.address}</div>
+        <div className="flex items-center gap-1">
+          <FiPhone />
+          {address.phone}
+        </div>
+      </div>
+      <Link href={"/account/settings/addresses/1"} className="text-primary">
+        Edit
+      </Link>
+    </div>
+  );
+}
