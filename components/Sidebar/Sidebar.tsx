5576f9a7b3d0d1eb3c47a1ca4e13e56ef7d80b26
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-05-07 00:12:41 +0500

    fix(Sidebar): fix page content from going behind sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -8,27 +8,27 @@ import SidebarProfile from "./SidebarProfile";
 
 export default function Sidebar({ children }: { children?: ReactNode }) {
   const [isOpen, setOpen] = useState(true);
-  console.log(isOpen);
 
   return (
-    <motion.aside
-      layout
-      layoutDependency={isOpen}
-      className={clsx(
-        "sidebar-container shrink-0 z-10 shadow-xl transition-all duration-300",
-        isOpen ? "relative" : "absolute",
-        "w-14 hover:w-[260px]",
-        isOpen && "w-[260px]"
-      )}
-    >
-      <div className="sidebar overflow-x-hidden overflow-y-auto h-screen bg-background">
-        <SidebarProfile
-          onSidebarToggle={() => setOpen(!isOpen)}
-          imgUrl="https://vetra.laborasyon.com/assets/images/user/man_avatar3.jpg"
-          name="Syed Hassan"
-        />
-        {children}
-      </div>
-    </motion.aside>
+    <div className={`${!isOpen && "mr-14"}`}>
+      <motion.aside
+        layout
+        layoutDependency={isOpen}
+        className={clsx(
+          "sidebar-container shrink-0 z-10 shadow-xl transition-all duration-300",
+          isOpen ? "relative w-[260px]" : "absolute",
+          "w-14 hover:w-[260px]"
+        )}
+      >
+        <div className="sidebar overflow-x-hidden overflow-y-auto h-screen bg-background">
+          <SidebarProfile
+            onSidebarToggle={() => setOpen(!isOpen)}
+            imgUrl="https://vetra.laborasyon.com/assets/images/user/man_avatar3.jpg"
+            name="Syed Hassan"
+          />
+          {children}
+        </div>
+      </motion.aside>
+    </div>
   );
 }
