2e54e19f955712ec4442c7b3a9f47228e5d13103
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-05-07 00:10:40 +0500

    refactor(ProfileForm): add validation to profile form using react-hook-form and yup

--- components/ProfileAvatarChanger.tsx
+++ components/ProfileAvatarChanger.tsx
@@ -1,61 +1,102 @@
 "use client";
 
-import React, { useRef, useState } from "react";
-import Button from "./Button";
 import Image from "next/image";
-import { FaCircleUser } from "react-icons/fa6";
+import { ChangeEvent, useRef, useState } from "react";
+import { useFormContext, useWatch } from "react-hook-form";
 import { BsTrash3 } from "react-icons/bs";
+import { FaCircleUser } from "react-icons/fa6";
+import Button from "./Button";
+import { UserProfile } from "./ProfileForm";
 
 interface Props {
   img?: string;
-  name: string;
 }
 
-export default function ProfileAvatarChanger({ img, name }: Props) {
+export default function ProfileAvatarChanger({ img }: Props) {
+  const {
+    register,
+    formState: { errors },
+    resetField,
+  } = useFormContext<UserProfile>();
+
+  const [firstName, lastName] = useWatch({
+    name: ["firstName", "lastName"],
+  });
+
   const [selectedImage, setSelectedImage] = useState<string | undefined>(img);
-  const ref = useRef<HTMLInputElement>(null);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const { ref, ...rest } = register("image", {
+    onChange: handleFileChange,
+  });
+
+  function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
+    console.log("changing");
+
+    const file = e.currentTarget.files?.[0];
+    if (file) {
+      setSelectedImage(URL.createObjectURL(file));
+    }
+  }
+
+  function deleteProfilePicture() {
+    resetField("image");
+    setSelectedImage(undefined);
+  }
   return (
-    <section className="flex justify-start items-center gap-2">
-      <div className="w-20 h-20 relative">
-        {selectedImage ? (
-          <Image
-            className="w-20 h-20 rounded-full object-center object-cover"
-            src={selectedImage}
-            fill
-            alt="Profile picture"
-          />
-        ) : (
-          <FaCircleUser className="w-20 h-20" />
-        )}
-      </div>
-      <div className="ml-4">
-        <h2 className="text-body-dark text-base font-semibold mb-4">{name}</h2>
-        <div className="flex gap-2 ">
-          <Button
-            onClick={() => {
-              ref.current?.click();
-            }}
-          >
-            <input
-              onChange={(e) => {
-                const file = e.currentTarget.files?.[0];
-                if (file) setSelectedImage(URL.createObjectURL(file));
-              }}
-              ref={ref}
-              className="opacity-0 w-0"
-              type="file"
+    <section className="">
+      <div className="flex justify-start items-center gap-2">
+        <div className="w-20 h-20 relative shrink-0">
+          {selectedImage ? (
+            <Image
+              className="w-20 h-20 rounded-full object-center object-cover"
+              src={selectedImage}
+              fill
+              alt="Profile picture"
             />
-            Change Avatar
-          </Button>
-          <Button
-            onClick={() => setSelectedImage(undefined)}
-            variant="outline"
-            color="red"
-          >
-            <BsTrash3 />
-          </Button>
+          ) : (
+            <FaCircleUser className="w-20 h-20" />
+          )}
+        </div>
+        <div className="ml-4">
+          <h2 className="text-body-dark text-base font-semibold mb-4">
+            {(firstName ?? "") + " " + (lastName ?? "")}
+          </h2>
+          <div className="flex gap-2 ">
+            <Button
+              onClick={() => {
+                inputRef.current?.click();
+              }}
+            >
+              <input
+                {...rest}
+                ref={(node) => {
+                  ref(node);
+                  inputRef.current = node;
+                  () => {
+                    ref(null);
+                    inputRef.current = null;
+                  };
+                }}
+                className="opacity-0 w-0"
+                type="file"
+              />
+              Change Avatar
+            </Button>
+            <Button
+              onClick={deleteProfilePicture}
+              variant="outline"
+              color="red"
+            >
+              <BsTrash3 />
+            </Button>
+          </div>
         </div>
       </div>
+      {errors.image && (
+        <p className="text-sm text-red mt-2 ">{errors.image.message}</p>
+      )}
     </section>
   );
 }
--- components/ProfileForm.tsx
+++ components/ProfileForm.tsx
@@ -1,31 +1,172 @@
+"use client";
 import Input from "./Input";
 import Button from "./Button";
+import { FormProvider, useForm } from "react-hook-form";
+import ProfileAvatarChanger from "./ProfileAvatarChanger";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const REQUIRED_MESSAGE = "This field is required";
+const SUPPORTED_IMG_TYPES = ["image/png", "image/jpeg", "image/webp"];
+const SIZE_2MB = 2097152;
+const profileSchema = yup.object({
+  image: yup
+    .mixed()
+    .transform((fileList) => (fileList as FileList).item(0))
+    .test({
+      message: "Invalid file type",
+      name: "Invalid Type",
+      test: (file, context) =>
+        file ? SUPPORTED_IMG_TYPES.includes((file as File).type) : true,
+    })
+    .test({
+      name: "File Size",
+      message: "File size should be less than 5MB",
+      test: (file, context) => (file ? (file as File).size < SIZE_2MB : true),
+    })
+    .notRequired(),
+  firstName: yup
+    .string()
+    .trim()
+    .max(100, "First Name can be maximum 100 chars")
+    .required(REQUIRED_MESSAGE),
+  lastName: yup
+    .string()
+    .trim()
+    .max(100, "Last Name can be maximum 100 chars")
+    .required(REQUIRED_MESSAGE),
+  age: yup
+    .number()
+    .transform((value) => (isNaN(value) ? null : value))
+    .positive()
+    .nullable(),
+  email: yup
+    .string()
+    .trim()
+    .email("Email is Invalid")
+    .required(REQUIRED_MESSAGE),
+  dateOfBirth: yup
+    .date()
+    .transform((value) => (isNaN(value) ? null : value))
+    .nullable(),
+  phone: yup.string().trim().optional(),
+  gender: yup
+    .mixed()
+    .oneOf(["male", "female", "other", "unknown"])
+    .required(REQUIRED_MESSAGE),
+});
+
+export type UserProfile = yup.InferType<typeof profileSchema>;
 
 export default function ProfileForm() {
+  const methods = useForm({
+    resolver: yupResolver(profileSchema),
+  });
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = methods;
+  console.log(errors);
+
+  function submitHandler(data: UserProfile): string {
+    console.log(data.image);
+    console.log("Logged Data: ", data);
+    return "Hello";
+  }
+
   return (
     <section className="bg-white rounded-lg p-6 pb-10 text-body-dark shadow-xl">
-      <h2 className="text-xl text-body-dark font-semibold mb-4">
-        Profile Info
-      </h2>
-      <form className="grid grid-cols-2 gap-4" action="">
-        <Input label="First Name" />
-        <Input label="Last Name" />
-        <Input label="Age" />
-        <Input type="date" label="Date of Birth" />
-        <Input type="email" label="Email" />
-        <Input label="Phone" />
-        <div className="col-span-2">
-          <p className="mb-1">Gender</p>
-          <div className="flex gap-4 items-end">
-            <Input type="radio" name="gender" label="Male" />
-            <Input type="radio" name="gender" label="Female" />
-            <Input type="radio" name="gender" label="Other" />
+      <FormProvider {...methods}>
+        <form
+          onSubmit={handleSubmit(submitHandler)}
+          className="grid grid-cols-2 gap-4"
+        >
+          <div className="col-span-2 mb-5">
+            <ProfileAvatarChanger />
           </div>
-        </div>
-        <Button className=" " color="green">
-          Save
-        </Button>
-      </form>
+          <div>
+            <Input {...register("firstName")} label="First Name" />
+            {errors.firstName && (
+              <p className="text-sm text-red">{errors.firstName.message}</p>
+            )}
+          </div>
+          <div>
+            <Input {...register("lastName")} label="Last Name" />
+            {errors.lastName && (
+              <p className="text-sm text-red">{errors.lastName.message}</p>
+            )}
+          </div>
+          <div>
+            <Input
+              {...register("age", { valueAsNumber: true })}
+              type="number"
+              label="Age"
+            />
+            {errors.age && (
+              <p className="text-sm text-red">{errors.age.message}</p>
+            )}
+          </div>
+          <div>
+            <Input
+              {...register("dateOfBirth", { valueAsDate: true })}
+              type="date"
+              label="Date of Birth"
+            />
+            {errors.dateOfBirth && (
+              <p className="text-sm text-red">{errors.dateOfBirth.message}</p>
+            )}
+          </div>
+          <div>
+            <Input {...register("email")} type="email" label="Email" />
+            {errors.email && (
+              <p className="text-sm text-red">{errors.email.message}</p>
+            )}
+          </div>
+          <div>
+            <Input {...register("phone")} label="Phone" />
+            {errors.phone && (
+              <p className="text-sm text-red">{errors.phone.message}</p>
+            )}
+          </div>
+          <div className="col-span-2">
+            <p className="mb-1">Gender</p>
+            <div className="flex gap-4 items-end">
+              <Input
+                {...register("gender")}
+                value={"male"}
+                defaultChecked
+                type="radio"
+                label="Male"
+              />
+              <Input
+                {...register("gender")}
+                value={"female"}
+                type="radio"
+                label="Female"
+              />
+              <Input
+                {...register("gender")}
+                value={"other"}
+                type="radio"
+                label="Other"
+              />
+              <Input
+                {...register("gender")}
+                value={"unknown"}
+                type="radio"
+                label="Prefer not to say"
+              />
+            </div>
+            {errors.gender && (
+              <p className="text-sm text-red">{errors.gender.message}</p>
+            )}
+          </div>
+          <Button type="submit" className=" " color="green">
+            Save
+          </Button>
+        </form>
+      </FormProvider>
     </section>
   );
 }
