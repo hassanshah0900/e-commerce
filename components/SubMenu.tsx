7db67fc8ab2ebc7fa3c589c0781b61525de4cdf3
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-05-07 00:14:52 +0500

    refactor(SubMenu): enable navigation using link instead of state

--- components/SubMenu.tsx
+++ components/SubMenu.tsx
@@ -1,40 +1,37 @@
 "use client";
 import clsx from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 export interface MenuItem {
-  value: string;
   label: string;
+  href: string;
   icon: ReactNode;
 }
 
 interface Props {
   menuItems: MenuItem[];
-  selectedMenu: MenuItem;
-  onMenuItemSelect: (menuItem: MenuItem) => void;
 }
 
-export default function SubMenu({
-  menuItems,
-  selectedMenu,
-  onMenuItemSelect,
-}: Props) {
+export default function SubMenu({ menuItems }: Props) {
+  const pathname = usePathname();
   return (
     <menu className="p-4 bg-white rounded-lg h-max">
       {menuItems.map((menuItem) => (
-        <li key={menuItem.value}>
-          <button
-            onClick={() => onMenuItemSelect(menuItem)}
+        <li key={menuItem.href}>
+          <Link
+            href={menuItem.href}
             className={clsx(
               "py-2 px-4 rounded-md transition-colors w-full text-start cursor-pointer flex items-center gap-2 focus-visible:outline  focus-visible:outline-primary",
-              selectedMenu === menuItem
+              pathname.startsWith(menuItem.href)
                 ? "bg-primary-light text-primary"
                 : "hover:text-body-dark focus-visible:text-body-dark"
             )}
           >
             <span className="text-base">{menuItem.icon}</span>
             {menuItem.label}
-          </button>
+          </Link>
         </li>
       ))}
     </menu>
