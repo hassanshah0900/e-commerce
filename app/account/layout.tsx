81d2d4452378f6ca06e3ab21dba977dc31bc48b3
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-05-07 14:58:53 +0500

    feat: create layout for account page

---
+++ app/account/layout.tsx
@@ -0,0 +1,41 @@
+import Sidebar from "@/components/Sidebar/Sidebar";
+import SidebarItem from "@/components/Sidebar/SidebarItem";
+import SidebarSection from "@/components/Sidebar/SidebarSection";
+import { LayoutGroup } from "motion/react";
+import React, { ReactNode } from "react";
+import { BsYoutube } from "react-icons/bs";
+import * as motion from "motion/react-client";
+
+export default function AccountLayout({ children }: { children: ReactNode }) {
+  return (
+    <main className="flex">
+      <LayoutGroup>
+        <Sidebar>
+          <SidebarSection title="Apps">
+            <SidebarItem href="/account/orders" icon={<BsYoutube />}>
+              orders
+            </SidebarItem>
+            <SidebarItem href="/account/settings" icon={<BsYoutube />}>
+              Settings
+            </SidebarItem>
+            <SidebarItem href="/hello" icon={<BsYoutube />}>
+              products
+            </SidebarItem>
+            <SidebarItem href="/hello" icon={<BsYoutube />}>
+              invoices
+            </SidebarItem>
+          </SidebarSection>
+        </Sidebar>
+
+        <motion.section
+          layout
+          transition={{ ease: "linear", duration: 0.15 }}
+          className="h-screen overflow-auto w-full"
+          style={{ scrollbarGutter: "stable" }}
+        >
+          {children}
+        </motion.section>
+      </LayoutGroup>
+    </main>
+  );
+}
