9401a76fc66e2d2e204d162f1040f7066f31b9ad
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-05-07 15:00:20 +0500

    feat: create settings page

---
+++ app/account/settings/layout.tsx
@@ -0,0 +1,33 @@
+import SubMenu, { MenuItem } from "@/components/SubMenu";
+import { ReactNode } from "react";
+import { CiUser, CiLocationOn } from "react-icons/ci";
+import { IoLockClosedOutline } from "react-icons/io5";
+
+const menuItems = [
+  {
+    label: "Profile",
+    icon: <CiUser />,
+    href: "/account/settings/profile",
+  },
+  {
+    label: "Password",
+    icon: <IoLockClosedOutline />,
+    href: "/account/settings/change-password",
+  },
+  {
+    label: "Addresses",
+    icon: <CiLocationOn />,
+    href: "/account/settings/addresses",
+  },
+];
+export default function SettingsLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="grid grid-cols-[2fr_1fr] gap-4 p-5">
+      {children}
+
+      <div className="sticky top-0 h-max">
+        <SubMenu menuItems={menuItems} />
+      </div>
+    </div>
+  );
+}
---
+++ app/account/settings/page.tsx
@@ -0,0 +1,9 @@
+import ProfileForm from "@/components/Profile/ProfileForm";
+
+export default async function SettingsPage() {
+  return (
+    <div>
+      <ProfileForm />
+    </div>
+  );
+}
