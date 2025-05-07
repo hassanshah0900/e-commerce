f09459b4fff2905e019dd66582e1a3963de2c7db
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-05-07 14:58:02 +0500

    chore: fix the typo

--- components/UpdatePasswordForm.tsx
+++ components/UpdatePasswordForm.tsx
@@ -5,7 +5,7 @@ export default function UpdatePasswordForm() {
   return (
     <section className="bg-white rounded-lg p-6 pb-10 text-body-dark shadow-xl">
       <h2 className="text-xl text-body-dark font-semibold mb-4">
-        Profile Info
+        Change Password
       </h2>
       <form className="space-y-4" action="">
         <Input label="Old Password" />
